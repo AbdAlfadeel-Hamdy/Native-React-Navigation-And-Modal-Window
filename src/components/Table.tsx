73131eaf78ea213c79bded74251723f62330ae1e
Abd Alfadeel Hamdy <abdalfadeelh@gmail.com>
2023-07-22 12:01:43 +0300

    add table

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,11 +1,40 @@
-import { DataProps } from "../pages/TablePage";
-
-interface Props {
-  data: DataProps[];
+export interface TableConfig {
+  label: string;
+  render: (dataItem: any) => any;
 }
 
-const Table = ({ data }: Props) => {
-  return <div>{data.length} </div>;
+interface Props {
+  data: any[];
+  config: TableConfig[];
+  keyFn: (rowData: any) => string;
+}
+
+const Table = ({ data, config, keyFn }: Props) => {
+  const renderedHeaders = config.map((column) => (
+    <th key={column.label}>{column.label}</th>
+  ));
+
+  const renderedRows = data.map((rowData: any) => {
+    const renderedCells = config.map((column) => (
+      <td key={column.label} className="p-2">
+        {column.render(rowData)}
+      </td>
+    ));
+    return (
+      <tr key={keyFn(rowData)} className="border-b">
+        {renderedCells}
+      </tr>
+    );
+  });
+
+  return (
+    <table className="table-auto border-spacing-2">
+      <thead>
+        <tr className="border-b-2">{renderedHeaders}</tr>
+      </thead>
+      <tbody>{renderedRows}</tbody>
+    </table>
+  );
 };
 
 export default Table;
--- src/pages/TablePage.tsx
+++ src/pages/TablePage.tsx
@@ -1,21 +1,37 @@
 import Table from "../components/Table";
 
-export interface DataProps {
+import { TableConfig } from "../components/Table";
+
+export interface TableData {
   name: string;
   color: string;
   score: number;
 }
 
 const TablePage = () => {
-  const data: DataProps[] = [
+  const data: TableData[] = [
     { name: "Orange", color: "bg-orange-500", score: 5 },
     { name: "Apple", color: "bg-red-500", score: 3 },
     { name: "Banana", color: "bg-yellow-500", score: 7 },
     { name: "Lime", color: "bg-green-500", score: 4 },
   ];
+  const config: TableConfig[] = [
+    {
+      label: "Name",
+      render: (fruit: TableData) => fruit.name,
+    },
+    {
+      label: "Color",
+      render: (fruit: TableData) => (
+        <div className={`p-3 m-2 ${fruit.color}`}></div>
+      ),
+    },
+    { label: "Score", render: (fruit: TableData) => fruit.score },
+  ];
+  const keyFn = (fruit: TableData) => fruit.name;
   return (
     <div>
-      <Table data={data} />
+      <Table data={data} config={config} keyFn={keyFn} />
     </div>
   );
 };
